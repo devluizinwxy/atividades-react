f3b085a4dc35335b2dc695ef287a259734eb8fac
Elizandra Fernandes <luis.f@academico.ifs.edu.br>
2026-01-29 01:03:27 -0300

    feat: atividade 01 - primeiro componente React

--- src/atividades/atividade2/app.jsx
+++ src/atividades/atividade2/app.jsx
@@ -1,7 +1,7 @@
 export default function Atividade02() {
   return (
     <div>
-      <h1>Atividade 01</h1>
+      <h1>Atividade asas 01</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Atividade1 from "./atividades/atividade1/app.jsx"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Atividade1 />
   </StrictMode>,
 )
