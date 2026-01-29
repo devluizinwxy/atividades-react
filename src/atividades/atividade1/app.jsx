089b8a110ffb301f8bf3f49f20f0bb662c143c29
Elizandra Fernandes <luis.f@academico.ifs.edu.br>
2026-01-28 23:49:51 -0300

    feat: cria estrutura da atividade 01

---
+++ src/atividades/atividade1/app.jsx
@@ -0,0 +1,7 @@
+export default function Atividade01() {
+  return (
+    <div>
+      <h1>Atividade 01</h1>
+    </div>
+  )
+}
---
+++ src/atividades/atividade2/app.jsx
@@ -0,0 +1,7 @@
+export default function Atividade02() {
+  return (
+    <div>
+      <h1>Atividade 01</h1>
+    </div>
+  )
+}
